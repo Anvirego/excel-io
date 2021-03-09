3f87bfd0f7f05301b52ea629425189da3bda0ccd
Anvirego <evee_chan@hotmail.com>
2021-03-08 20:26:43 -0600

    pom

--- target/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.regav"},{"l":"com.regav.interfaces"}];updateSearchResults();
--- target/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"com.regav","l":"Excel"},{"p":"com.regav","l":"ExcelIdem"},{"p":"com.regav.interfaces","l":"ExcelInterface"}];updateSearchResults();
